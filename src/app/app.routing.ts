c5d2ae262e34efe7019267bff695cce9952e5e22
Leah Sherrell <leahsherrell@gmail.com>
2017-07-14 15:11:28 -0700

    add scouts and about components with routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,21 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './base/base.component';
+import { AboutComponent }   from './about/about.component';
+import { ScoutsComponent }   from './scouts/scouts.component';
 
 const appRoutes: Routes = [
   {
   path: '',
   component: BaseComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'scouts',
+    component: ScoutsComponent
   }
 ];
 
